b434c084de069fe02f02839dc51dafdd1737684e
Osvaldo Rojas <osvaldoc@gmail.com>
2021-07-06 15:54:58 -0500

    osc¿valdo

--- JS/Login2.js
+++ JS/Login2.js
@@ -1,5 +1,5 @@
 function usuarios() {
-    var ListaUsuarios = JSON-parseFloat(localStorage.getItem('LUsuarios'));
+    var ListaUsuarios = JSON.parseFloat(localStorage.getItem('LUsuarios'));
 
     if (ListaUsuarios == null) {
 
@@ -13,6 +13,23 @@ function usuarios() {
             6° Fecha
             7° Rol = 1) Administrador 2) Usuario
         */
-        ListaUsuarios = ['1','Osvaldo', 'Sanchez', 'osvaldoc2704@gmail.com', 'nodejes4915', '1997-04-27', '1']
+        ListaUsuarios = ['1','Osvaldo', 'Sanchez', 'osvaldo@gmail.com', 'exgames01', '1997-04-27', '1'] , ['1','Miriam', 'Rivera', 'miriam@gmail.com', 'exgames02', '1997-04-27', '1'] ,['1','armando', 'legos', 'mandolegos@gmail.com', 'lego21', '1997-04-27', '2']
     }
+    return ListaUsuarios;
+}
+
+function validcion(inCorreo, inContra) {
+    var ListaUsuarios = usuarios();
+    var accesoU = false;
+    
+    for (var i = 0; i < ListaUsuarios.length; i++) {
+        if (inCorreo == ListaUsuarios[i][3] && inContra == ListaUsuarios[i][4]){
+            accesoU = true;
+            localStorage.setItem('UsuarioAct', ListaUsuarios[i][1] + ListaUsuarios[i][2]);
+            localStorage.setItem('RolUsuario', ListaUsuarios[i][6]);
+        
+        }
+        
+    }
+    return accesoU;
 }
